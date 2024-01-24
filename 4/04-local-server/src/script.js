d77dde2a9047994e4ed0bb2472c90b4ac407daf6
Stefan Roets <stefanroetscode@gmail.com>
2024-01-24 09:52:03 +0200

    first commit

---
+++ 4/04-local-server/src/script.js
@@ -0,0 +1,32 @@
+import * as THREE from "three"
+
+// Scene
+const scene = new THREE.Scene()
+
+// Red cube
+const geometry = new THREE.BoxGeometry(1, 1, 1,)
+const material = new THREE.MeshBasicMaterial ({ color: 0xff0000 })
+const mesh = new THREE.Mesh (geometry, material)
+scene.add(mesh)
+
+// Size
+const size = {
+    width: 800,
+    height: 600
+}
+
+// Camera (point of view)
+const camera = new THREE.PerspectiveCamera(75, size.width / size.height)
+camera.position.z = 3
+camera.position.y = 1
+
+scene.add(camera)
+
+// Renderer
+const canvas = document.querySelector('.webgl')
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(size.width, size.height)
+
+renderer.render(scene, camera)
