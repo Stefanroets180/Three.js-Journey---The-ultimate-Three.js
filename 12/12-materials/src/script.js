7993af7d5546e63ce2a03c09201e1231f24654e8
Stefan Roets <stefanroetscode@gmail.com>
2024-01-30 13:34:21 +0200

    chapter12 Objects/Materials

--- 12/12-materials/src/script.js
+++ 12/12-materials/src/script.js
@@ -2,15 +2,6 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 /**
- * Base
- */
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
-
-// Scene
-const scene = new THREE.Scene()
-
-/**
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
@@ -22,12 +13,45 @@ const doorHeightTexture = textureLoader.load('/textures/door/height.jpg')
 const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg')
 const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
 const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
-const matcapTexture = textureLoader.load('/textures/matcaps/1.png')
+const matcapTexture = textureLoader.load('/textures/matcaps/7.png')
 const gradientTexture = textureLoader.load('/textures/gradients/3.jpg')
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
 /**
  * Objects
+ * 1 mesh basic material
+ * 2 mesh normal material
+ * 3 mesh matcap material
+ * 4 mesh depth material
  */
-const material = new THREE.MeshBasicMaterial()
+// const material = new THREE.MeshBasicMaterial()
+// material.map = doorColorTexture
+// //material.color.set ('#ff00ff')
+// //material.color = new THREE.Color('green')
+// //material.wireframe = true
+// //material.opacity = 0.5
+// material.transparent = true
+// material.alphaMap = doorAlphaTexture
+// material.side = THREE.DoubleSide
+
+// const material = new THREE.MeshNormalMaterial()
+// material.wireframe=true
+// material.flatShading=true
+
+//const material = new THREE.MeshMatcapMaterial()
+// material.matcap = matcapTexture
+const material = new THREE.MeshDepthMaterial()
+
+
+
 
 const sphere = new THREE.Mesh(
     new THREE.SphereGeometry(0.5, 16, 16),
@@ -47,6 +71,14 @@ const torus = new THREE.Mesh(
 torus.position.x = 1.5
 
 scene.add(sphere, plane, torus)
+
+/**
+ * Light
+//  */
+// const ambientLight = new THREE.AmbientLight(0xffff, 0.5)
+// scene.add(ambientlight)
+//
+// const point
 
 /**
  * Sizes
@@ -102,6 +134,16 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    //Update object
+    sphere.rotation.y = 0.1 * elapsedTime
+    plane.rotation.y = 0.1 * elapsedTime
+    torus.rotation.y = 0.1 * elapsedTime
+
+    sphere.rotation.x = 0.15 * elapsedTime
+    plane.rotation.x = 0.15 * elapsedTime
+    torus.rotation.x = 0.15 * elapsedTime
+
 
     // Update controls
     controls.update()
