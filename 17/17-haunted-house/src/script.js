9afcbd034c3978e623bf15c01175733a406608f7
Stefan Roets <stefanroetscode@gmail.com>
2024-02-01 18:49:40 +0200

    chapter17 Ghost light

--- 17/17-haunted-house/src/script.js
+++ 17/17-haunted-house/src/script.js
@@ -206,6 +206,18 @@ doorLight.position.set(0, 2.2, 2.7)
 house.add(doorLight)
 
 /**
+ * Ghosts
+ */
+const ghost1 = new THREE.PointLight('#ff00ff', 2, 3)
+scene.add(ghost1)
+
+const ghost2 = new THREE.PointLight('#00ffff', 2, 3)
+scene.add(ghost2)
+
+const ghost3 = new THREE.PointLight('#ffff00', 2, 3)
+scene.add(ghost3)
+
+/**
  * Sizes
  */
 const sizes = {
@@ -253,6 +265,17 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 renderer.setClearColor('#262837')
 
 /**
+ * Shadows
+ */
+renderer.shadowMap.enabled = true
+
+moonLight.castShadow = true
+doorLight.castShadow = true
+ghost1.castShadow = true
+ghost2.castShadow = true
+ghost3.castShadow = true
+
+/**
  * Animate
  */
 const clock = new THREE.Clock()
@@ -260,6 +283,24 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    //Update Ghost
+    const ghost1Angle = elapsedTime * 0.5
+    ghost1.position.x = Math.cos(ghost1Angle) * 4
+    ghost1.position.z = Math.sin(ghost1Angle) * 4
+    ghost1.position.y = Math.sin(elapsedTime * 3)
+
+    const ghost2Angle = elapsedTime * 0.32
+    ghost2.position.x = Math.cos(ghost2Angle) * 5
+    ghost2.position.z = Math.sin(ghost2Angle) * 5
+    ghost2.position.y = Math.sin(elapsedTime * 4) + Math.sin(elapsedTime * 2.5)
+
+    const ghost3Angle = elapsedTime * 0.5
+    ghost3.position.x = Math.cos(ghost3Angle) *  (7 + Math.sin(elapsedTime * 0.32))
+    ghost3.position.z = Math.sin(ghost3Angle) * (7 + Math.sin(elapsedTime * 0.5))
+    ghost3.position.y = Math.sin(elapsedTime * 3) + Math.sin(elapsedTime * 2.5)
+
+
 
     // Update controls
     controls.update()
