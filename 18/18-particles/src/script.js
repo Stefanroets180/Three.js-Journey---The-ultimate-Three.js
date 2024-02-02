ff239493c3fbe2eda370f7976f6321969a00006b
Stefan Roets <stefanroetscode@gmail.com>
2024-02-02 13:12:20 +0200

    chapter18 Animate(done)

--- 18/18-particles/src/script.js
+++ 18/18-particles/src/script.js
@@ -129,6 +129,20 @@ const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
 
+    // Update particles
+    //particles.rotation.y = elapsedTime * 0.2
+
+    for(let i = 0; i < count; i++)
+    {
+        const i3 = i * 3
+        const x = particlesGeometry.attributes.position.array[i3]
+        particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + x)
+
+    }
+
+    particlesGeometry.attributes.position.needsUpdate = true
+
+
     // Update controls
     controls.update()
 
