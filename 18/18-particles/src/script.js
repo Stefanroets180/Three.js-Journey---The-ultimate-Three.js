6da1248e806c415c1c9a3a3e9c259d74e0c46a58
Stefan Roets <stefanroetscode@gmail.com>
2024-02-02 12:59:57 +0200

    chapter18 Animate(start)

--- 18/18-particles/src/script.js
+++ 18/18-particles/src/script.js
@@ -26,18 +26,26 @@ const particleTexture = textureLoader.load('/textures/particles/2.png')
  */
 // Geometry
 const particlesGeometry = new THREE.BufferGeometry()
-const count = 5000
+const count = 20000
 
 const positions = new Float32Array(count * 3) // Multiply by 3 because each position is composed of 3 values (x, y, z)
+const colors = new Float32Array(count * 3)
+
 
 for(let i = 0; i < count * 3; i++) // Multiply by 3 for same reason
 {
     positions[i] = (Math.random() - 0.5) * 10 // Math.random() - 0.5 to have a random value between -0.5 and +0.5
+    colors[i] = Math.random()
+
 }
 
 particlesGeometry.setAttribute(
     'position',
     new THREE.BufferAttribute(positions, 3))
+
+particlesGeometry.setAttribute(
+    'color',
+    new THREE.BufferAttribute(colors, 3))
 
 // Create the Three.js
 
@@ -47,23 +55,26 @@ particlesGeometry.setAttribute(
 const particlesMaterial = new THREE.PointsMaterial()
 particlesMaterial.size = 0.1
 particlesMaterial.sizeAttenuation = true
-particlesMaterial.color = new THREE.Color('#ff88cc')
+//particlesMaterial.color = new THREE.Color('#ff88cc')
 particlesMaterial.transparent = true
 particlesMaterial.alphaMap = particleTexture
 //particlesMaterial.alphaTest = 0.001
-particlesMaterial.depthTest = false
+//particlesMaterial.depthTest = false
+particlesMaterial.depthWrite = false
+particlesMaterial.blending = THREE.AdditiveBlending
+particlesMaterial.vertexColors = true
 
 
 // Points
 const particles = new THREE.Points(particlesGeometry, particlesMaterial)
 scene.add(particles)
 
-// Cube
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(),
-    new THREE.MeshBasicMaterial()
-)
-scene.add(cube)
+// // test Cube
+// const cube = new THREE.Mesh(
+//     new THREE.BoxGeometry(),
+//     new THREE.MeshBasicMaterial()
+// )
+// scene.add(cube)
 
 /**
  * Sizes
