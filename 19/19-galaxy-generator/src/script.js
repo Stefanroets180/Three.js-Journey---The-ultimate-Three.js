997c8bb8bcd3db74099fd1ea4e93dcf3cfecf2ed
Stefan Roets <stefanroetscode@gmail.com>
2024-02-04 11:09:44 +0200

    chapter19 (Tweaks Fix)

--- 19/19-galaxy-generator/src/script.js
+++ 19/19-galaxy-generator/src/script.js
@@ -6,7 +6,7 @@ import * as dat from 'lil-gui'
  * Base
  */
 // Debug
-const gui = new dat.GUI()
+const gui = new dat.GUI({ width: 360})
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -19,16 +19,27 @@ const scene = new THREE.Scene()
  */
 
 const parameters = {}
-parameters.count = 1000
-parameters.size = 0.02
+parameters.count = 100000
+parameters.size = 0.01
 
+let geometry = null
+let material = null
+let points = null
 
 const generateGalaxy = () =>
 {
+    // Destroy old galaxy
+    if(points !== null)
+    {
+        geometry.dispose()
+        material.dispose()
+        scene.remove(points)
+    }
+
     /**
      * Geometry
      */
-    const geometry = new THREE.BufferGeometry()
+    geometry = new THREE.BufferGeometry()
 
     const positions = new Float32Array(parameters.count * 3)
 
@@ -46,7 +57,7 @@ const generateGalaxy = () =>
     /**
      * Material
      */
-    const material = new THREE.PointsMaterial({
+    material = new THREE.PointsMaterial({
         size: parameters.size,
         sizeAttenuation: true,
         depthWrite: false,
@@ -56,11 +67,13 @@ const generateGalaxy = () =>
     /**
      * Points
      */
-    const points = new THREE.Points(geometry, material)
+    points = new THREE.Points(geometry, material)
     scene.add(points)
 }
 generateGalaxy()
 
+gui.add(parameters, 'count').min(100).max(1000000).step(100).onFinishChange(generateGalaxy)
+gui.add(parameters, 'size').min(0.001).max(0.1).step(0.001).onFinishChange(generateGalaxy)
 /**
  * Sizes
  */
