78b7f181b00300851968c037c39468785fc76d48
Stefan Roets <stefanroetscode@gmail.com>
2024-03-02 13:42:50 +0200

    chapter41 (React normal done)

--- 41/41-first-react-application/src/index.jsx
+++ 41/41-first-react-application/src/index.jsx
@@ -1,8 +1,11 @@
-import './style.css'
 import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 
 const root = createRoot(document.querySelector('#root'))
 
 root.render(
-    <h1>Hello React</h1>
+    <App clickersCount={ 3 }>
+        <h1>My First React App</h1>
+        <h2>And a fancy subtitle</h2>
+    </App>
 )
