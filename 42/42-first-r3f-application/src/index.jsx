0d502e73537291e83c8d7da3bb3466e8109bedb8
Stefan Roets <stefanroetscode@gmail.com>
2024-03-04 12:21:16 +0200

    chapter42 (React with three fiber Refactoring)

--- 42/42-first-r3f-application/src/index.jsx
+++ 42/42-first-r3f-application/src/index.jsx
@@ -1,16 +1,15 @@
 import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
+import Experience from "./Experience.jsx";
+
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
     <>
         <Canvas>
-            <mesh>
-                <torusKnotGeometry/>
-                <meshNormalMaterial/>
-            </mesh>
+            <Experience/>
         </Canvas>
     </>
 )
