37a9082f7f714df18bb31bd3e7b9e8182c969c6f
Stefan Roets <stefanroetscode@gmail.com>
2024-03-05 16:58:31 +0200

    chapter44 (Debug a R3F Application Range and Vectors)

--- 44/44-debug-a-r3f-application/src/Experience.jsx
+++ 44/44-debug-a-r3f-application/src/Experience.jsx
@@ -1,8 +1,17 @@
 import { OrbitControls } from '@react-three/drei'
-import Cube from "./Cube.jsx";
+import { useControls } from 'leva'
 
 export default function Experience()
 {
+    const {position} = useControls({
+        position:
+            {
+                value: { x: -2, y: 0},
+                step: 0.01,
+                joystick: 'invertY'
+            }
+    })
+
     return <>
 
         <OrbitControls makeDefault />
@@ -10,12 +19,15 @@ export default function Experience()
         <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
         <ambientLight intensity={ 0.5 } />
 
-        <mesh position-x={ - 2 }>
+        <mesh position={ [position.x, position.y, 0] }>
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
         </mesh>
 
-        <Cube scale={1.5}/>
+        <mesh position-x={ 2 } scale={ 1.5 }>
+            <boxGeometry />
+            <meshStandardMaterial color="mediumpurple" />
+        </mesh>
 
         <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
             <planeGeometry />
