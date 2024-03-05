b29c67f1d8bd1b273d11894a81154729cf1653a3
Stefan Roets <stefanroetscode@gmail.com>
2024-03-05 10:52:35 +0200

    chapter43 (React with three fiber and Drei fix OrbitControls conflict)

--- 43/43-r3f-drei/src/Experience.jsx
+++ 43/43-r3f-drei/src/Experience.jsx
@@ -1,16 +1,11 @@
-import { useThree, extend } from '@react-three/fiber'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-
-extend({ OrbitControls })
-
+import {TransformControls, OrbitControls} from "@react-three/drei"
+import {useRef} from "react"
 export default function Experience()
 {
-    const { camera, gl } = useThree()
+    const cube = useRef()
 
     return <>
-
-        <orbitControls args={ [ camera, gl.domElement ] } />
-
+        <OrbitControls makeDefault />
         <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
         <ambientLight intensity={ 0.5 } />
 
@@ -19,10 +14,12 @@ export default function Experience()
             <meshStandardMaterial color="orange" />
         </mesh>
 
-        <mesh position-x={ 2 } scale={ 1.5 }>
+        <mesh ref={cube} position-x={ 2 } scale={ 1.5 }>
             <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
         </mesh>
+        <TransformControls object={cube} />
+
 
         <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
             <planeGeometry />
