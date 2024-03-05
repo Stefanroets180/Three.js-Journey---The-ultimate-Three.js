c9fa13035f8ca7cf5118a7791316203945ea56fb
Stefan Roets <stefanroetscode@gmail.com>
2024-03-05 18:11:19 +0200

    chapter44 (Debug a R3F Application done)

--- 44/44-debug-a-r3f-application/src/Experience.jsx
+++ 44/44-debug-a-r3f-application/src/Experience.jsx
@@ -1,8 +1,14 @@
 import { OrbitControls } from '@react-three/drei'
 import { button, useControls } from 'leva'
+import {Perf} from "r3f-perf";
+
 
 export default function Experience()
 {
+    const { perfVisible } = useControls({
+        perfVisible: true
+    })
+
     const {position, color, visible} = useControls('sphere',{
         position:
             {
@@ -22,19 +28,31 @@ export default function Experience()
         choice: { options: ['a', 'b', 'c'] }
     })
 
+    const { scale } = useControls('çube',{
+        scale:
+            {
+                value: 1.5,
+                step: 0.01,
+                min: 0,
+                max: 5
+            }
+    })
+
     return <>
+
+        { perfVisible && <Perf position="top-left" /> }
 
         <OrbitControls makeDefault />
 
         <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
         <ambientLight intensity={ 0.5 } />
 
-        <mesh visible={visible} position={ [position.x, position.y, 0] }>
+        <mesh position={ [position.x, position.y, 0] } visible={visible}>
             <sphereGeometry />
             <meshStandardMaterial color={ color } />
         </mesh>
 
-        <mesh position-x={ 2 } scale={ 1.5 }>
+        <mesh position-x={ 2 } scale={ scale }>
             <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
         </mesh>
--- 44/44-debug-a-r3f-application/src/index.jsx
+++ 44/44-debug-a-r3f-application/src/index.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience.jsx'
 import React, {StrictMode} from "react";
+import {Leva} from "leva";
 
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
     <StrictMode>
+        <Leva collapsed/>
         <Canvas
             camera={ {
                 fov: 45,
