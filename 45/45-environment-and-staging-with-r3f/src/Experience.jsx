a7f5d04ff82ea2f8c7cc8fa123a5859f22fb5f87
Stefan Roets <stefanroetscode@gmail.com>
2024-03-06 14:41:45 +0200

    chapter45 (Environment and Staging Default shadows Activation )

--- 45/45-environment-and-staging-with-r3f/src/Experience.jsx
+++ 45/45-environment-and-staging-with-r3f/src/Experience.jsx
@@ -26,20 +26,20 @@ export default function Experience()
 
         //Light Helper
 
-        <directionalLight ref={directionalLight} position={[1, 2, 3]} intensity={1.5}/>
+        <directionalLight ref={directionalLight} castShadow position={[1, 2, 3]} intensity={1.5}/>
         <ambientLight intensity={0.5}/>
 
-        <mesh position-x={-2}>
+        <mesh castShadow position-x={-2}>
             <sphereGeometry/>
             <meshStandardMaterial color="orange"/>
         </mesh>
 
-        <mesh ref={cube} position-x={2} scale={1.5}>
+        <mesh castShadow ref={cube} position-x={2} scale={1.5}>
             <boxGeometry/>
             <meshStandardMaterial color="mediumpurple"/>
         </mesh>
 
-        <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+        <mesh receiveShadow position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
             <planeGeometry/>
             <meshStandardMaterial color="greenyellow"/>
         </mesh>
