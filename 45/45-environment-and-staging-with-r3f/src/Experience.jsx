9579936c3254931a7e350e114f75420dd97fc8cc
Stefan Roets <stefanroetscode@gmail.com>
2024-03-06 12:14:22 +0200

    chapter45 (Environment and Staging Color)

--- 45/45-environment-and-staging-with-r3f/src/Experience.jsx
+++ 45/45-environment-and-staging-with-r3f/src/Experience.jsx
@@ -1,6 +1,6 @@
 import { useFrame } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
-import { useRef } from 'react'
+import React, { useRef } from 'react'
 import { Perf } from 'r3f-perf'
 
 export default function Experience()
@@ -14,26 +14,28 @@ export default function Experience()
 
     return <>
 
-        <Perf position="top-left" />
+        <color args={['#81D8D0']} attach="background"/>
 
-        <OrbitControls makeDefault />
+        <Perf position="top-left"/>
 
-        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-        <ambientLight intensity={ 0.5 } />
+        <OrbitControls makeDefault/>
 
-        <mesh position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
+        <directionalLight position={[1, 2, 3]} intensity={1.5}/>
+        <ambientLight intensity={0.5}/>
+
+        <mesh position-x={-2}>
+            <sphereGeometry/>
+            <meshStandardMaterial color="orange"/>
         </mesh>
 
-        <mesh ref={ cube } position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
+        <mesh ref={cube} position-x={2} scale={1.5}>
+            <boxGeometry/>
+            <meshStandardMaterial color="mediumpurple"/>
         </mesh>
 
-        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
+        <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+            <planeGeometry/>
+            <meshStandardMaterial color="greenyellow"/>
         </mesh>
 
     </>
--- 45/45-environment-and-staging-with-r3f/src/index.jsx
+++ 45/45-environment-and-staging-with-r3f/src/index.jsx
@@ -2,18 +2,33 @@ import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience.jsx'
+import React from "react";
+import * as THREE from 'three'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
+// //Setting color on the renderer
+// const created = ({gl}) =>
+// {
+//     gl.setClearColor('#ff0000', 1)
+// }
+
+// //With the scene background
+// const created = ({ scene }) =>
+// {
+//     scene.background = new THREE.Color('#ff0000')
+// }
 
 root.render(
     <Canvas
-        camera={ {
+        camera={{
             fov: 45,
             near: 0.1,
             far: 200,
-            position: [ - 4, 3, 6 ]
-        } }
+            position: [-4, 3, 6]
+        }}
+        // onCreated={ created } //with the scene background//
     >
-        <Experience />
+
+        <Experience/>
     </Canvas>
 )
