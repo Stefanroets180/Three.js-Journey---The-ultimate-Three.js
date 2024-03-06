c6f7e2c398836f79b1def72ce6f955dfa064a433
Stefan Roets <stefanroetscode@gmail.com>
2024-03-06 17:09:39 +0200

    chapter46 (Load Models with R3F DRACO)

--- 46/46-load-models-with-r3f/src/Experience.jsx
+++ 46/46-load-models-with-r3f/src/Experience.jsx
@@ -2,10 +2,20 @@ import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
 import { useLoader} from '@react-three/fiber';
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 
 export default function Experience()
 {
-    const model = useLoader(GLTFLoader, './hamburger.glb')
+    const model = useLoader(
+        GLTFLoader,
+        './hamburger-draco.glb',
+        (loader) =>
+        {
+            const dracoLoader = new DRACOLoader()
+            dracoLoader.setDecoderPath('./draco/')
+            loader.setDRACOLoader(dracoLoader)
+        }
+    )
 
     return <>
 
@@ -21,7 +31,7 @@ export default function Experience()
             <meshStandardMaterial color="greenyellow" />
         </mesh>
 
-        <primitive object={ model.scene } />
+        <primitive object={ model.scene } scale={0.35} />
 
     </>
 }
