502da0943a84adce2e629da80fddaa1ab21816de
Stefan Roets <stefanroetscode@gmail.com>
2024-03-07 13:36:09 +0200

    chapter46 (Load Models with R3F Preloading)

--- 46/46-load-models-with-r3f/src/Model.jsx
+++ 46/46-load-models-with-r3f/src/Model.jsx
@@ -1,9 +1,8 @@
 import { useGLTF } from '@react-three/drei'
-/*import { useLoader} from '@react-three/fiber'
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js'
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'*/
+
 
 export default function model () {
     const model = useGLTF('./hamburger-draco.glb')
     return <primitive object={model.scene} scale={0.35} />
 }
+useGLTF.preload('./hamburger-draco.glb')
