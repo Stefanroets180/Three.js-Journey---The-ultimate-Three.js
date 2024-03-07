c781d996ea93aca269b976c3e7a84067201258ca
Stefan Roets <stefanroetscode@gmail.com>
2024-03-07 13:26:11 +0200

    chapter46 (Load Models with R3F GLTF Loading with Drei)

--- 46/46-load-models-with-r3f/src/Model.jsx
+++ 46/46-load-models-with-r3f/src/Model.jsx
@@ -1,18 +1,9 @@
-import { useLoader} from '@react-three/fiber'
+import { useGLTF } from '@react-three/drei'
+/*import { useLoader} from '@react-three/fiber'
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js'
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'*/
 
 export default function model () {
-    const model = useLoader(
-        GLTFLoader,
-        './FlightHelmet/glTF/FlightHelmet.gltf',
-
-        //'./hamburger-draco.glb',
-        (loader) => {
-            const dracoLoader = new DRACOLoader()
-            dracoLoader.setDecoderPath('./draco/')
-            loader.setDRACOLoader(dracoLoader)
-        }
-    )
-    return <primitive object={model.scene} scale={5} position-y={-1}/>
+    const model = useGLTF('./hamburger-draco.glb')
+    return <primitive object={model.scene} scale={0.35} />
 }
