674753d7d4d0042a73dc434cd4dd8a8a5dd3bc4c
Stefan Roets <stefanroetscode@gmail.com>
2024-03-08 16:51:03 +0200

    chapter47 (3D Text with R3F DONE)

--- 47/47-3d-text-with-r3f/src/Experience.jsx
+++ 47/47-3d-text-with-r3f/src/Experience.jsx
@@ -1,8 +1,46 @@
-import {Text3D, OrbitControls, Center} from '@react-three/drei'
+import { useMatcapTexture, Center, Text3D, OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
+import { useEffect, useRef, useState } from 'react'
+import { useFrame } from '@react-three/fiber'
+import * as THREE from 'three'
+
+const torusGeometry = new THREE.TorusGeometry(1, 0.6, 16, 32)
+const material = new THREE.MeshMatcapMaterial()
+const materiall = new THREE.MeshMatcapMaterial()
 
 export default function Experience()
 {
+    const donuts = useRef([])
+    const [matcapTexture] = useMatcapTexture('46804D_CBE9AC_90B57C_95D38F', 256)
+    const [matcapTexture2] = useMatcapTexture('537387_75BBB9_152E5B_0E85E8\n', 256)
+
+
+    useFrame((state, delta) =>
+    {
+        for(const donut of donuts.current)
+        {
+            donut.rotation.y += delta * 0.2
+        }
+    })
+
+    useEffect(() =>
+    {
+        matcapTexture.encoding = THREE.sRGBEncoding
+        matcapTexture.needsUpdate = true
+
+        material.matcap = matcapTexture
+        material.needsUpdate = true
+    }, [])
+
+    useEffect(() =>
+    {
+        matcapTexture2.encoding = THREE.sRGBEncoding
+        matcapTexture2.needsUpdate = true
+
+        materiall.matcap = matcapTexture2
+        materiall.needsUpdate = true
+    }, [])
+
     return <>
 
         <Perf position="top-left" />
@@ -10,19 +48,43 @@ export default function Experience()
         <OrbitControls makeDefault />
 
         <Center>
-            <Text3D font="./fonts/Phlegmy Kilmister_Regular.json"
-                    size={ 0.75 }
-                    height={ 0.2 }
-                    curveSegments={ 12 }
-                    bevelEnabled
-                    bevelThickness={ 0.02 }
-                    bevelSize={ 0.002 }
-                    bevelOffset={ 0 }
-                    bevelSegments={ 5 }>
-                HELLO R3F
-                <meshNormalMaterial/>
+            <Text3D
+                material={ materiall }
+                font="./fonts/Phlegmy Kilmister_Regular.json"
+                size={ 0.75 }
+                height={ 0.2 }
+                curveSegments={ 12 }
+                bevelEnabled
+                bevelThickness={ 0.002 }
+                bevelSize={ 0.002 }
+                bevelOffset={ 0 }
+                bevelSegments={ 5 }
+            >
+                STEFAN TEST 12345
             </Text3D>
         </Center>
+
+        { [...Array(100)].map((value, index) =>
+            <mesh
+                ref={ (element) => donuts.current[index] = element }
+                key={ index }
+                geometry={ torusGeometry }
+                material={ material }
+                position={ [
+                    (Math.random() - 0.5) * 10,
+                    (Math.random() - 0.5) * 10,
+                    (Math.random() - 0.5) * 10
+                ] }
+                scale={ 0.2 + Math.random() * 0.2 }
+                rotation={ [
+                    Math.random() * Math.PI,
+                    Math.random() * Math.PI,
+                    0
+                ] }
+            >
+                <torusGeometry args={ [ 1, 0.6, 16, 32 ] } />
+            </mesh>
+        ) }
 
     </>
 }
