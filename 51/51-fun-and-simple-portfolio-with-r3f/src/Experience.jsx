af3da2cb649c16446b08cd15ed7e93d23269fa1c
Stefan Roets <stefanroetscode@gmail.com>
2024-03-20 14:15:50 +0200

    chapter51 (Fun and Simple Portfolio with R3F Spring Configuration )

--- 51/51-fun-and-simple-portfolio-with-r3f/src/Experience.jsx
+++ 51/51-fun-and-simple-portfolio-with-r3f/src/Experience.jsx
@@ -1,15 +1,27 @@
-import { OrbitControls } from '@react-three/drei'
+import { PresentationControls,Float,Environment,useGLTF,  } from '@react-three/drei'
+import Macbook from './Macbook.jsx'
 
 export default function Experience()
 {
     return <>
+        <Environment preset="city"/>
 
-        <OrbitControls makeDefault />
-        
-        <mesh>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
+        <color args={ [ '#241a1a' ] } attach="background" />
+
+        <PresentationControls
+            global
+            rotation={[0.13,0.1,0]}
+            polar={[-0.4,0.2]}
+            azimuth={ [ - 1, 0.75 ] }
+            config={ { mass: 2, tension: 400 } }
+        >
+        <Float rotationIntensity={0.4}>
+            <Macbook
+            position-y={-1.2}
+        />
+        </Float>
+        </PresentationControls>
+
 
     </>
 }
