40037af7ada6653d23f1486f39a027c4973b9f69
Stefan Roets <stefanroetscode@gmail.com>
2024-03-22 12:32:36 +0200

    chapter51 (Fun and Simple Portfolio with R3F done )

--- 51/51-fun-and-simple-portfolio-with-r3f/src/Experience.jsx
+++ 51/51-fun-and-simple-portfolio-with-r3f/src/Experience.jsx
@@ -1,4 +1,4 @@
-import { PresentationControls,Float,Environment,useGLTF,  } from '@react-three/drei'
+import { Text, Html, ContactShadows, PresentationControls, Float, Environment, useGLTF,  } from '@react-three/drei'
 import Macbook from './Macbook.jsx'
 
 export default function Experience()
@@ -10,17 +10,70 @@ export default function Experience()
 
         <PresentationControls
             global
-            rotation={[0.13,0.1,0]}
+            rotation={[0.2,0.05,0]}
             polar={[-0.4,0.2]}
-            azimuth={ [ - 1, 0.75 ] }
+            azimuth={ [ - 1, 0.75] }
             config={ { mass: 2, tension: 400 } }
+            snap={ { mass: 4, tension: 400 } }
         >
-        <Float rotationIntensity={0.4}>
-            <Macbook
-            position-y={-1.2}
-        />
-        </Float>
+            <Float rotationIntensity={0.2}>
+                <rectAreaLight
+                    width={2.5}
+                    height={2.65}
+                    intensity={95}
+                    color={'#7611a6'}
+
+                    rotation={[-0.1, Math.PI, 0]}
+                    position={[0, 0.55, -1.15]}
+                />
+
+                <Macbook
+                    position-y={-1.4}
+                />
+
+                <Html
+                    transform
+                    wrapperClass="htmlScreen"
+                    distanceFactor={1.12}
+                    position={[0, 0.1, -1.4]}
+                    rotation-x={-0.256}
+
+                >
+                    <iframe src="https://stefan-portfolio-86.deno.dev/"/>
+                </Html>
+
+                <rectAreaLight
+                    width={2.5}
+                    height={2.65}
+                    intensity={95}
+                    color={'#7611a6'}
+
+                    rotation={[-0.1, Math.PI, 0]}
+                    position={[-20, 0, 1.15]}
+                />
+
+                <Text
+                    font="./bangers-v20-latin-regular.woff"
+                    fontSize={1}
+                    position={[2, 0.75, 0.75]}
+                    rotation-y={-1.25}
+                    maxWidth={2}
+                    color={'#296575'}
+                >
+                    STEFAN
+                    Roets
+                </Text>
+
+
+            </Float>
         </PresentationControls>
+
+        <ContactShadows
+            position-y={-1.4}
+            opacity={0.4}
+            scale={5}
+            blur={2.4}
+        />
 
 
     </>
--- 51/51-fun-and-simple-portfolio-with-r3f/src/index.jsx
+++ 51/51-fun-and-simple-portfolio-with-r3f/src/index.jsx
@@ -11,7 +11,7 @@ root.render(
             fov: 45,
             near: 0.1,
             far: 2000,
-            position: [ -3, 1.5, 4 ]
+            position: [ -1, 1.5, 4 ]
         } }
     >
         <Experience />
